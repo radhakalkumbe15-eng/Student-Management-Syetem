b0f488ab3f1e8fcd74825720dc8bbf3c571b891a
radhafirstgit <radhakalkumbe15@gmail.com>
2026-09-18 02:06:25 +0530

    Add gitignore and update DBConnection

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -7,7 +7,7 @@ public class DBConnection {
         try{
             String url = "jdbc:mysql://localhost:3306/student_management";
             String username="root";
-            String password="MYSQL@1234";
+            String password="YOUR_MYSQL_PASSWORD";
             Class.forName("com.mysql.cj.jdbc.Driver");
             connection=DriverManager.getConnection(url,username,password);
             System.out.println("database connected successfully!");
